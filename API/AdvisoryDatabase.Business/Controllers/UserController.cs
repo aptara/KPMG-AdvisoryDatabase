ee514688b9ab6a737aec746a69be7c2572bb82f8
shivaniGhewari <shiwanighewari2704@gmail.com>
2023-05-18 21:15:34 +0530

    remained

---
+++ API/AdvisoryDatabase.Business/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdvisoryDatabase.Business.Service;
+using AdvisoryDatabase.Framework.Entities;
+using AdvisoryDatabase.Framework.Logger;
+
+namespace AdvisoryDatabase.Business.Controllers
+{
+    public class UserController : BaseController
+    {
+        
+
+        public List<User> GetUserDetailsByUserID(User ObjInputParameters)
+        {
+            List<User> Userdata = new List<User>();
+            try
+            {
+                UserService service = new UserService();
+                Userdata = service.GetAll();
+
+            }
+            catch (Exception ex)
+            {
+
+                Log4NetLogger.Error(ex.Message);
+            }
+
+            return Userdata;
+        }
+
+      
+
+
+    }
+
+
+}
+
+
+
+
