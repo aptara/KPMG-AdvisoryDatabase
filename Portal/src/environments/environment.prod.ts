5521201e767207abfa64c8cc50e478c19d7264e8
pushprajjagadale <pushpraj.jagadale@aptaracorp.com>
2023-07-10 11:04:53 +0530

    Prod

--- Portal/src/environments/environment.prod.ts
+++ Portal/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    baseURL: 'http://ctsdemo.aptaracorp.net/advisorydatabase/',
-    baseUrl: 'http://ctsdemo.aptaracorp.net//AdvisoryDatabaseAPI/API/'
+    baseURL: 'http://ctsdemo.aptaracorp.net/advisorydatabasedev/',
+    baseUrl: 'https://ctsdemo.aptaracorp.net/AdvisoryDatabaseAPIDev/API/'
 };
